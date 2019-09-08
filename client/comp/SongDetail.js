8b59610bb6e1b269db8c8842ac0b019fba316893
armjim14 <ajimenez1497@gmail.com>
2019-09-07 23:32:12 -0700

    song lyrics showing

--- client/comp/SongDetail.js
+++ client/comp/SongDetail.js
@@ -3,18 +3,22 @@ import { graphql } from 'react-apollo';
 import SingleSong from '../queries/SingleSong'
 
 class SongDetail extends Component {
+
+    getComments() {
+        return this.props.data.song.lyrics.map( ar => { return <li key={ar.id}>{ar.content}</li> } )
+    }
     
     render() {
-
-        // console.log(this.props)
-        console.log(this.props.data.loading)
         if (this.props.data.loading) { return <div>Loading...</div> }
 
         return (
             <div>
                 <h3>Song Detail</h3>
-                <p>{ this.props.data.song.title }</p>
-
+                <p>Title: <b>{ this.props.data.song.title }</b></p>
+                <hr />
+                <ol>
+                    {this.getComments()}
+                </ol>
             </div>
         )
     }
